660204b91eb87a4845c6b7ba17f03cea6ac49f5e
Rafael Monico <rmonico@moningrado>
2022-10-09 21:31:05 -0300

    test: Testes do módulo dado (não passando)

---
+++ src/banco_imobiliario/dado.py
@@ -0,0 +1,10 @@
+"""
+Módulo de dado
+"""
+
+def lancar() -> int:
+    """
+    Sorteia um número aleatório entre 1 e 6
+    """
+
+    return -1
---
+++ tests/banco_imobiliario_tests/test_dado.py
@@ -0,0 +1,26 @@
+"""
+Testes do módulo dado
+"""
+from unittest import TestCase
+from banco_imobiliario import dado
+
+
+class DadoTestCase(TestCase):
+    """
+    Testes do módulo dado
+    """
+
+    def test_lancar_dado(self):
+        """
+        QUANDO o dado for lançado
+        DEVE retornar um número inteiro aleatório entre 1 e 6
+        """
+
+        for _ in range(10000):
+            resultado = dado.lancar()
+
+            self.assertTrue(resultado >= 1)
+            self.assertTrue(resultado <= 6)
+
+            # TODO Somar a quantidade de vezes que cada resultado é obtido para verificar se a
+            # quantidade de vezes é próximo a 1/6 para cada um
